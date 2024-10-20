76e9247a9fc65f40bd3bb2e8da7d7b92cce7b055
sreij <chrj23020@g.nihon-u.ac.jp>
2024-10-20 22:19:34 +0900

    nameの保持

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 
 export default function App() {
   const [name, setName] = useState("Bob");
+  const [newName, setNewName] = useState("");
   const [showStory, setShow] = useState(false);
   const insertX = [["Willy the Goblin"],["Big Daddy"],["Father Christmas"]];
   const insertY = [["the soup kitchen"],["Disneyland"],["the White House"]];
@@ -9,13 +10,15 @@ export default function App() {
   const [xItem, setX] = useState("");
   const [yItem, setY] = useState("");
   const [zItem, setZ] = useState("");
-  const ukus = "us";
+  const [ukus, setUKUS] = useState("us");
+  const [temp, setTemp] = useState("fahrenheit")
 
   function randomValueFromArray(array){
     const random = Math.floor(Math.random()*array.length);
     return array[random];
   }
 
+  /*
   function result(event){
     if(event.target.value !== ""){
       setName(event.target.value);
@@ -23,24 +26,30 @@ export default function App() {
       setName("Bob");
     }
   }
+    */
   
-  function buttonClicked() {
+  function buttonClicked(event) {
     setShow(true);
     setX(randomValueFromArray(insertX));
     setY(randomValueFromArray(insertY));
     setZ(randomValueFromArray(insertZ));
+    if(newName !== ""){
+      setName(newName);
+    }else{
+      setName("Bob");
+    }
   }
   return (
     <>
       <div>
         <label htmlFor="customname">Enter custom name:</label>
-        <input type="text" placeholder=""  onChange={result} />
+        <input type="text" placeholder="" onChange={(event) => {setNewName(event.target.value)}}/>
       </div>
       <div>
         <label htmlFor="us">US</label>
-        <input type="radio" value="us" checked={ukus === "us"} />
+        <input type="radio" value="us" checked={ukus === "us"} onChange={() => setUKUS("us")}/>
         <label htmlFor="uk">UK</label>
-        <input type="radio" value="uk" checked={ukus === "uk"} />
+        <input type="radio" value="uk" checked={ukus === "uk"} onChange={() => setUKUS("uk")}/>
       </div>
       <div>
         <button onClick={buttonClicked}>Generate random story</button>
